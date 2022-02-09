32086e5b3623fdcd77ed10b881659c62adbde4f1
Vanesa <mv382038@gmail.com>
2022-02-09 15:04:29 -0300

    uso fileSystem

--- routes/products.js
+++ routes/products.js
@@ -2,54 +2,37 @@ const multer = require('multer')
 const express = require('express')
 const path = require('path')
 const { Router } = express
+const Contenedor = require('./contenedor');
+const instancia = new Contenedor("./routes/products.json");
 
+//instancia.getAll().then(products => console.log(products));
 let id = 0;
+
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, path.join(__dirname, ("../public/img")))
     },
     filename: (req, file, cb) => {
-        cb(null, file.fieldname +'-'+ Date.now())
+        cb(null, file.originalname)
     }
 })
 
 const upload = multer({ storage })
 const router = Router()
 
-products = [
-    {
-        id: id+=1,
-        title: 'Detergente',
-        price: 240,
-        thumbnail: "imagen1"
-    },
-    {
-        id: id+=1,
-        title: 'Alcohol',
-        price: 220,
-        thumbnail: "imagen2"
-    },
-    {
-        id: id+=1,
-        title: 'Papas',
-        price: 100,
-        thumbnail: "imagen3"
-    }
-]
-
-router.get("/", (req, res) =>{
-    //res.send(movies.filter(m => m.title.includes(title)))
-    res.status(200).send(products)
+router.get("/", async (req, res) =>{
+    const productos = await instancia.getAll().then(products => {return products});
+    res.status(200).send(productos)
 })
 
 router.get("/nuevo", (req, res)=>{
     res.sendFile(path.join(__dirname, "../public/productos.html"))
 })
 
-
-router.get("/:id", (req, res) =>{
-    const product = products.find(product => product.id == req.params.id)
+router.get("/:id", async (req, res) =>{
+    const product = await instancia.getById(req.params.id).then(product => {return product});
+    //const product = products.find(product => product.id == req.params.id)
     if(!product){
         res.status(404).send({ error : 'Producto no encontrado' })
         return
@@ -59,17 +42,22 @@ router.get("/:id", (req, res) =>{
 
 //POST
 
-router.post("/", upload.single("thumbnail"), (req, res) =>{
-    id+=1;
-    const thumbnail = path.join(__dirname, "../public/img/"+ req.file.filename)
+router.post("/", upload.single("thumbnail"), async (req, res) =>{
+    const thumbnail = path.join(__dirname, "../public/img/"+ req.file)
     const { title, price } = req.body
-    products.push({id, title, price, thumbnail})
-    res.send({id, title, price, thumbnail})
+    //products.push({id, title, price, thumbnail})
+    id +=1;
+    console.log(id)
+    const product = await instancia.save({id, title, price, thumbnail})
+    if(product){
+        res.status(201).send(product);
+    }
 })
 
 //PUT
 router.put("/:id", (req, res) =>{
-    const product = products.find(product => product.id == req.params.id)
+    const product = await instancia.update(product).then(product => {return product});
+    //const product = products.find(product => product.id == req.params.id)
     if(!product){
         res.status(404).send({ error : 'Producto no encontrado' })
         return
@@ -83,14 +71,14 @@ router.put("/:id", (req, res) =>{
 
 //DELETE
 router.delete("/:id", (req, res) =>{
-    const product = products.find(product => product.id == req.params.id)
-    if(!product){
+    const productDelete = instancia.deleteById(req.params.id);
+    if(!productDelete){
         res.status(404).send({ error : 'Producto no encontrado' })
         return
     }
-    const index = products.indexOf(movie)
-    products.splice(index, 1)
     res.sendStatus(200)
 })
 
 module.exports = router
+
+//res.send(movies.filter(m => m.title.includes(title)))
