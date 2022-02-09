427a64226700cdb8d46b310a5702a68d31e929c5
Vanesa <mv382038@gmail.com>
2022-02-09 15:58:08 -0300

    Valido id

--- routes/products.js
+++ routes/products.js
@@ -6,7 +6,6 @@ const Contenedor = require('../models/contenedor');
 const instancia = new Contenedor("./database/products.json");
 
 //instancia.getAll().then(products => console.log(products));
-let id = 0;
 
 
 const storage = multer.diskStorage({
@@ -23,7 +22,7 @@ const router = Router()
 
 router.get("/", async (req, res) =>{
     const productos = await instancia.getAll().then(products => {return products});
-    res.status(200).send(productos)
+    res.status(200).send(productos);
 })
 
 router.get("/nuevo", (req, res)=>{
@@ -32,20 +31,31 @@ router.get("/nuevo", (req, res)=>{
 
 router.get("/:id", async (req, res) =>{
     const product = await instancia.getById(req.params.id).then(product => {return product});
-    //const product = products.find(product => product.id == req.params.id)
     if(!product){
         res.status(404).send({ error : 'Producto no encontrado' })
         return
     }
-    res.send(product)
+    res.send(product);
 })
 
 //POST
+
+const validacionId = async () =>{
+    let id = 1;
+    const productos = await instancia.getAll().then(products => {return products});
+    if(productos.length > 0){
+        const index = productos.length -1;
+        const producto = productos[index];
+        id = producto.id + 1;
+        return id;
+    }
+    return id;
+}
+
 router.post("/", upload.single("thumbnail"), async (req, res) =>{
     const thumbnail = path.join(__dirname, "../public/img/"+ req.file)
-    const { title, price } = req.body
-    //products.push({id, title, price, thumbnail})
-    id +=1;
+    const { title, price } = req.body;
+    let id = await validacionId();
     const product = await instancia.save({id, title, price, thumbnail})
     if(product){
         res.status(201).send(product);
@@ -74,7 +84,7 @@ router.delete("/:id", (req, res) =>{
         res.status(404).send({ error : 'Producto no encontrado' })
         return
     }
-    res.sendStatus(200)
+    res.sendStatus(200);
 })
 
 module.exports = router
